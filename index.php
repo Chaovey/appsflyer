7264f8e428007a6200d638be7010aa9e9a2eea90
Visal <123046820+webdemo55@users.noreply.github.com>
2023-10-20 09:26:01 +0700

    [update]

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php
-   header('Content-Type: application/json');
-   $data = [
-       'url'=>'https://www.youtube.com/'
-   ];
-   echo json_encode($data);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AppsFlyer</title>
+</head>
+<body>
+    <?php
+        header('Content-Type: application/json');
+        $data = [
+            'url'=>'https://www.youtube.com/'
+        ];
+        echo json_encode($data);
+    ?>
+</body>
+</html>
