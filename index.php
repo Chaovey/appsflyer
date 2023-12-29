379588014863bd18061dbf143cd3c10a2e621148
Chaovey Visal <140946219+Chaovey@users.noreply.github.com>
2023-12-29 11:13:34 +0700

    Update index.json
    second commit

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AppsFlyer</title>
-</head>
-<body>
-    <?php
-        header('Content-Type: application/json');
-        $data = [
-            'url'=>'https://www.youtube.com/'
-        ];
-        $result=json_encode($data);
-        echo $result
-    ?>
-</body>
-</html>
+{"status":200}
